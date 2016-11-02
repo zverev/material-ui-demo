d2f4b74211b850a666ab3cb8bba37776e69524f7
zverev <alexander.zverev@gmail.com>
2016-11-02 20:17:09 +0300

    use autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const path = require('path')
 
 const extractCSS = new ExtractTextPlugin('bundle.css')
+const autoprefixer = require('autoprefixer')
 
 module.exports = {
     entry: './src/entry.js',
@@ -14,8 +15,8 @@ module.exports = {
     module: {
         loaders: [
             { test: /\.js$/, loader: 'babel', query: { presets: ['es2015', 'react', 'stage-0'] }, exclude: /(node_modules|react-css-themr)/ },
-            { test: /\.css$/, loader: extractCSS.extract(['css']) },
-            { test: /\.(scss|sass)$/, loader: extractCSS.extract(['css?modules&sourceMap', 'sass?sourceMap']) },
+            { test: /\.css$/, loader: extractCSS.extract(['css', 'postcss-loader']) },
+            { test: /\.(scss|sass)$/, loader: extractCSS.extract(['css?modules&sourceMap', 'postcss-loader', 'sass?sourceMap']) },
             { test: /\.html$/, loader: 'raw' },
             { test: /\.(png|svg|jpg|ttf|eot|woff|woff2)$/, loader: 'file' }
         ]
@@ -28,6 +29,10 @@ module.exports = {
     plugins: [
         extractCSS
     ],
+
+    postcss: function () {
+      return [autoprefixer]
+    },
 
     resolve: { fallback: path.join(__dirname, "node_modules") },
     resolveLoader: { fallback: path.join(__dirname, "node_modules") },
